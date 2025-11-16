4dbd2d98abe1ae7f15768e6a5f14fbd9cc12ecdd
K.Sukumar <146643923+Suku8008m@users.noreply.github.com>
2025-11-16 18:00:07 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,8 +28,9 @@ export const register = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
+      path:'/',
       maxAge: 7 * 24 * 60 * 60 * 1000, //7 days to expire
     });
     //Sunding welcome Email start
@@ -73,8 +74,9 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      secure: true,
+      sameSite: "none",
+      path:"/",
       maxAge: 7 * 24 * 60 * 60 * 1000, //7 days to expire
     });
     return res.json({ success: true });
