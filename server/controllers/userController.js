b899c2bb6f0408b0b56fece5e490b0f9472e11f0
K.Sukumar <146643923+Suku8008m@users.noreply.github.com>
2025-11-18 23:35:41 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import userModel from "../models/userModel.js";
 
 export const getUserData = async (req, res) => {
   const { userId } = req.body;
+  return userId?res.send(userId+"USER_ID"):res.send("no user id")
   try {
     const user = await userModel.findById(userId);
     if (!user) {
